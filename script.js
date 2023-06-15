15129df3a9f69d33cbf20a3032e57204c1cdde68
Fclaudiobriito <fclaudio.briito@gmail.com>
2023-06-15 20:40:31 -0300

    busca cep

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 const findEstado = () => {
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
         .then(response => response.json())
-        .then(json => {
-            //console.log(json)
-            let inputEstado = document.getElementById('estado')
+        .then(data => {
+            data.sort((a, b) => (a.nome > b.nome) ? 1: -1)
+            
+            let estados = ''
+            data.forEach(e => estados += `<option value=${e.sigla}>${e.nome}</option>`);
+            //console.log(estados)
 
-            json.forEach(estado => {
-                console.log(estado)
+            uf.innerHTML = estados;
+
             })
 
-        })
-    }
+        }
 
 findEstado()
+
+const findCep = () => { 
+    let cepValue = cep.value
+    console.log(cepValue)
+    fetch(`https://viacep.com.br/ws/${cepValue}/json/`)
+        .then(response => response.json())
+        .then(json => {
+            preencheInput(json)
+
+        })
+}
+
+const preencheInput = json => {
+    numero.focus()
+    logradouro.value = json.logradouro
+    bairro.value = json.bairro
+    uf.value = json.uf
+    localidade.value = json.localidade
+}
